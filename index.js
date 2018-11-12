1b4040a5824b91d0478b94610a7e257f06d83a2e
satoshi eric <stevexia1@gmail.com>
2018-11-11 23:36:06 -0500

    updated CRUD

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get("/create", function(req,res){
     res.render("create.ejs");
 })
 
+app.get("/edit/name/:name/author/:author/handle/:handle/id/:id", function(req,res){
+    var data = {
+        name: req.params.name,
+        author: req.params.author,
+        handle: req.params.handle,
+        id: req.params.id
+    }
+    res.render("edit.ejs", {data:data});
+})
+
 app.post("/create/book",function(req,res){
     var data = {
         name: req.body.name,
@@ -35,6 +45,35 @@ app.post("/create/book",function(req,res){
     books.push(newBook);
     res.render("home.ejs", {data:data, books: books})
 })
+
+
+app.get("/delete/id/:id", function(req,res){
+    
+    for(var i=0; i<books.length;i++){
+        if(books[i].id==req.params.id){
+            books.splice(i, 1);
+        }
+    }
+    res.render("home.ejs",{books:books});
+})
+app.post("/edit/book",function(req,res){
+    var data = {
+        name: req.body.name,
+        author: req.body.author,
+        handle: req.body.handle,
+        id: req.body.id
+    }
+
+    for(var i=0; i<books.length;i++){
+        if(books[i].id==data.id){
+            books[i] = data
+        }
+    }
+
+    console.log(data)
+    res.render("home.ejs", {data:data, books: books})
+})
+
 app.post("/authenticate",function(req,res){
 
     console.log(req.body);
